c2313b08826353de7692530ffa34dc5e51a81a9b
Alexander Link <alexanderlink@web.de>
2018-09-20 19:41:43 +0200

    Added toggle function

--- listen.py
+++ listen.py
@@ -4,24 +4,37 @@ from scapy.all import sniff, ARP
 from datetime import datetime, timedelta
 from phue import Bridge
 
+last_state = "off"
+
 def arp_received(packet):
     if packet[ARP].op == 1 and packet[ARP].hwdst == '00:00:00:00:00:00':
         # Replace the Dash button addresses with yours
         if packet[ARP].hwsrc.upper() == 'FC:65:DE:81:25:74':  # Pampers
             print("Pampers Button pressed!")
-            setHueWickelnStart()
+            toggle()
+
+        elif packet[ARP].hwsrc.upper() == '50:F5:DA:3B:1B:30':  # Finish
+            print("Finish Button pressed!")
+            toggle()
 
         elif packet[ARP].hwsrc.upper() == 'FC:A6:67:B9:B8:75':  # Durex
             print("Durex Button pressed!")
 
-        elif packet[ARP].hwsrc.upper() == '50:F5:DA:3B:1B:30':  # Finish
-            print("Finish Button pressed!")
-            setHueWickelnStop()
+
+def toggle():
+    print("toggle lights")
+    global last_state
+    if last_state == 'off':
+        setOn()
+        last_state = 'on'
+    else:
+        setOff()
+        last_state = 'off'
 
 # Replace the Hue light names with yours and implement the desired logic.
 # initHue() below prints the list of lights.
-def setHueWickelnStart():
-    print("Hue Wickeln Start")
+def setOn():
+    print("setOn")
     b = initHue()
     b.set_light('Flur 1', 'on', True)
     b.set_light('Flur 1', 'bri', 10)
@@ -36,8 +49,8 @@ def setHueWickelnStart():
     b.set_light('Küche Lightstrip', 'on', True)
     b.set_light('Küche Lightstrip', 'bri', 20)
 
-def setHueWickelnStop():
-    print("Hue Wickeln Stop")
+def setOff():
+    print("setOff")
     b = initHue()
     b.set_light('Flur 1', 'on', False)
     b.set_light('Flur 2', 'on', False)
@@ -53,6 +66,7 @@ def initHue():
     b.connect()
     b.get_api()
     lights = b.lights
+    print("Available lights:")
     for l in lights:
         print(l)
     return b    
